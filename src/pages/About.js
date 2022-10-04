19d3c87ac65d2381b7791c50039f0dea9abae2fb
slandath <jesus_slanda@hotmail.com>
2022-10-03 23:58:19 -0400

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>Created by Rob Kazirut and Tom Slanda</p>
+      <a href="https://github.com/slandath/mtgCollection" target="blank">Github Repo</a>
+    </div>
+  );
+}
