40f9ea547171b23fdb24aad6a9260ade698be685
slandath <jesus_slanda@hotmail.com>
2022-11-27 00:06:34 -0500

    ripped out mapping and switched to exact search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./pages/Header";
 import Scryfall from "./pages/Scryfall";
-import CardResults from "./pages/CardResults"
 // import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 // import Sidebar from "./pages/Sidebar";
 // import Content from "./pages/Content";
 import Login from "./pages/Login";
 import Account from "./pages/Account";
+import CardResults from "./pages/CardResults";
 
 function App() {
   return (
@@ -29,6 +29,16 @@ function App() {
           <div className="account">
             <Header />
             <Account />
+            <Footer />
+          </div>
+        }
+      />
+      <Route
+        path="/versions"
+        element={
+          <div className="results-container">
+            <Header />
+            <CardResults />
             <Footer />
           </div>
         }
